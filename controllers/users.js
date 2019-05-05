1c486abaa80e351b0a0f5e72935d2102c895a9c8
Jake <mjakerick@gmail.com>
2019-05-05 10:25:41 -0700

    sign up and log in buttons work

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const user = express.Router()
+const users = express.Router()
 const User = require('../models/users.js')
 
-user.get('/new', (req, res) => {
+users.get('/new', (req, res) => {
   res.render('users/new.ejs')
 })
 
-module.exports = user
+users.post('/', (req, res) => {
+  User.create(req.body, (err, createdUser) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log(createdUser);
+    res.redirect('/')
+  })
+})
+
+module.exports = users
--- server.js
+++ server.js
@@ -1,36 +1,60 @@
-// dependencies
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+// use dotenv to import configs from the .env file
 require('dotenv').config()
-const methodOverride = require('method-override');
 
-const userController = require('./controllers/users.js')
-app.use('/users', userController)
-const sessionsController = require('./controllers/sessions.js')
-app.use('/sessions', sessionsController)
+// Dependencies
+const express = require('express')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const app = express()
 
-// config
+const session = require('express-session')
+
+// Configuration
 const PORT = process.env.PORT
 const mongoURI = process.env.MONGODB_URI
 
-//creates req.body
-app.use(methodOverride('_method'));
-// allows DELETE/PUT requests
-app.use(express.urlencoded({extended:false}));
+// Set Middleware
+// allow us to use put and delete methods
+app.use(methodOverride('_method'))
+// parses info from our input fields into an object
+app.use(express.urlencoded({ extended: false }))
 
-// Routes
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
-
-// listen
-app.listen(3000, () => {
-  console.log('listening....');
-});
+// configure sessions
+// secret is stored in .env
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
 
 // Database config and connection
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
   console.log('connected to mongo')
 })
+
+// Listen
+app.listen(PORT, () => console.log('auth happening on port', PORT))
+
+// Routes
+app.get('/', (req, res) => {
+  // res.send('index route')
+  res.render('index.ejs', {
+    currentUser: req.session.currentUser
+  })
+})
+
+app.get('/app', (req, res)=>{
+    if(req.session.currentUser){
+        res.render('app/index.ejs')
+    } else {
+        res.redirect('/sessions/new');
+    }
+})
+// Users controller - creates new users
+const userController = require('./controllers/users.js')
+app.use('/users', userController)
+
+// Sessions controller - handles user sessions
+const sessionsController = require('./controllers/sessions.js')
+app.use('/sessions', sessionsController)
