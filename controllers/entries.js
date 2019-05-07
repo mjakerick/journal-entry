ac41b94e15914f7c4d4c37a7e0e21e06ff6fd5d5
Jake <mjakerick@gmail.com>
2019-05-07 08:48:54 -0700

    Created a delete route to delete entries

--- controllers/entries.js
+++ controllers/entries.js
@@ -2,8 +2,8 @@ const express = require('express');
 const entries = express.Router();
 const Entry = require('../models/entries.js');
 
-entries.get('/new', (req, res)=>{
-    res.render('app/entries/new.ejs');
+entries.get('/new', (req, res) => {
+  res.render('app/entries/new.ejs')
 })
 
 module.exports = entries;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require('express')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
-
 const session = require('express-session')
+const Entry = require('./models/entries.js');
 
 // Configuration
 const PORT = process.env.PORT
@@ -44,13 +44,56 @@ app.get('/', (req, res) => {
   })
 })
 
+// Gets entries from database and renders them on index page
 app.get('/app', (req, res)=>{
     if(req.session.currentUser){
-        res.render('app/index.ejs')
+      Entry.find({}, (error, allEntries) => {
+          res.render('app/entries/index.ejs',{
+              entries:allEntries
+          });
+      })
     } else {
         res.redirect('/sessions/new');
     }
 })
+
+app.get('/new', (req, res)=>{
+  res.render('app/entries/new.ejs')
+})
+
+app.post('/entries', (req, res)=>{
+  Entry.create(req.body, (err, createdEntry) => {
+    res.redirect('/app')
+  })
+})
+
+app.get('/entries/:id', (req, res) => {
+    Entry.findById(req.params.id, (error, foundEntry) => {
+        res.render('app/entries/show.ejs', {
+            entry: foundEntry
+        });
+    });
+});
+
+app.get('/entries/:id/edit', (req, res) => {
+    Entry.findById(req.params.id, (err, foundEntry) => {
+        res.render('app/entries/edit.ejs',{
+            entry:foundEntry
+        });
+    })
+});
+
+app.put('/entries/:id', (req, res) => {
+    Entry.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel) => {
+        res.redirect('/app');
+    })
+});
+
+app.delete('/entries/:id', (req, res) => {
+    Entry.findByIdAndRemove(req.params.id, (err, data) => {
+        res.redirect('/app');
+    });
+});
 
 // Users controller - creates new users
 const userController = require('./controllers/users.js')
@@ -59,3 +102,7 @@ app.use('/users', userController)
 // Sessions controller - handles user sessions
 const sessionsController = require('./controllers/sessions.js')
 app.use('/sessions', sessionsController)
+
+// Entries controller - creates new entries
+const entriesController = require('./controllers/entries.js')
+app.use('/entries', entriesController)
